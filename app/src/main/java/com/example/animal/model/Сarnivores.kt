044de1eb2cd5d1b5a9106f216d3e600e2d62cb98
Valeriy <epifanovvaleriy@gmail.com>
2022-11-16 12:27:54 +0300

    Init

--- app/src/main/java/com/example/animal/model/Сarnivores.kt
+++ app/src/main/java/com/example/animal/model/Сarnivores.kt
@@ -2,5 +2,9 @@ package com.example.animal.model
 
 class Сarnivores(view:String, var weight: Double, var speed: Float): Animal(view) {
 
+ open  fun info(view:String): String{
+   return "$view weighs $weight a kilogram and run at speed: $speed"
+   }
+
  }
-   
+
--- app/src/main/java/com/example/animal/start/Main.kt
+++ app/src/main/java/com/example/animal/start/Main.kt
@@ -1,19 +1,21 @@
 package com.example.animal.start
 import com.example.animal.model.Сarnivores
 import com.example.animal.model.Herbivorous
+import kotlinx.coroutines.awaitAll
+import kotlin.reflect.full.memberProperties
 
 
 fun main (){
-    val arrayAnimal:MutableList<Herbivorous> = mutableListOf(Herbivorous("wild horse", range = "Mongolia, Kazakhstan, China and Russia"),
-        Herbivorous("elephant", range = "in the forests of East Asia and Africa, in the savannas"))
+    val arrayHerbivorousAnimal:MutableList<Herbivorous> = mutableListOf()
+    val arrayPredatorAnimals:MutableList<Сarnivores> = mutableListOf(Сarnivores("crocodile", weight=60.4, speed=30.7f),  Сarnivores("tiger", weight=25.4, speed=50.7f ))
 
-    val arrayAnimal2:MutableList<Сarnivores> = mutableListOf(Сarnivores("crocodile", weight=60.4, speed=30.7f),
-    Сarnivores("tiger", weight=25.4, speed=50.7f ))
+    var horse = Herbivorous("wild horse", "Mongolia Kazakhstan, China and Russia")
+    var elephan = Herbivorous("elephant", "in the forests of East Asia and Africa, in the savannas")
 
-    println(arrayAnimal)
-    println(arrayAnimal2)
+    arrayHerbivorousAnimal.add(horse)
+    arrayHerbivorousAnimal.add(elephan)
 
-
-
+    arrayHerbivorousAnimal.forEach { println(it.view + " usually living " + it.range) }
+    arrayPredatorAnimals.forEach{println(it.info(it.view))}
 
 }
